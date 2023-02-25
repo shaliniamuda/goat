12ff0bfa55ac99e2911dbedad3a974b2aafa80ea
shaliniamuda <126292305+shaliniamuda@users.noreply.github.com>
2023-02-25 16:36:20 +0530

    Update settings.py

--- amu/settings.py
+++ amu/settings.py
@@ -117,11 +117,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
 
 
 
