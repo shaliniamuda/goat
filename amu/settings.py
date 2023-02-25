abf4c8ea23eebb5b3ac809a923a1c29d0163bc9a
shaliniamuda <126292305+shaliniamuda@users.noreply.github.com>
2023-02-25 14:48:26 +0530

    Update settings.py

--- amu/settings.py
+++ amu/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hjip4603^tf@^sfc%t(1y=5#2w-#k2q1ao*jt8s*ko2udwl%qa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
