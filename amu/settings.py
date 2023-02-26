009abb96aac4bcdb8e6c830a1447b730dea17282
shaliniamuda <126292305+shaliniamuda@users.noreply.github.com>
2023-02-26 09:55:05 +0530

    Update settings.py

--- amu/settings.py
+++ amu/settings.py
@@ -118,13 +118,9 @@ USE_TZ = True
 import os
 
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
-
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 
